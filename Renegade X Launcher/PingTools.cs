d8220860395bdab4d97ccc829b421e2bc020c78c
StealthEye <stealtheye>
2015-02-22 18:58:48 +0000

    Remove unused public IP fetching.

--- Renegade X Launcher/PingTools.cs
+++ Renegade X Launcher/PingTools.cs
@@ -23,34 +23,5 @@ namespace LauncherTwo
                 return null;
             }
         }
-
-        public static async Task<IPAddress> GetPublicIPAddress()
-        {
-            try
-            {
-                using (var webClient = new WebClientWithTimeout(1000))
-                {
-                    /*
-                    string response = await webClient.DownloadStringTaskAsync("http://checkip.dyndns.org/");
-                    int first = response.IndexOf("Address: ") + 9;
-                    int last = response.LastIndexOf("</body>");
-                    string ipString = response.Substring(first, last - first);
-                    return IPAddress.Parse(ipString);
-                    */
-                    string response = await webClient.DownloadStringTaskAsync(RenXWebLinks.IP_PROVIDER_URL);
-                    int first = response.IndexOf("<body>");
-                    int last = response.LastIndexOf("</body>");
-                    if (first != -1 && last != -1)
-                    {
-                        response = response.Substring(first + 6, last - first - 6).Trim();
-                    }
-                    return IPAddress.Parse(response);
-                }
-            }
-            catch
-            {
-                return null;
-            }
-        }
     }
 }
