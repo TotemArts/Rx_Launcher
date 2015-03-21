1e59c4908c7ab9b55e17dc5067cfd6951bb612ee
StealthEye <stealtheye>
2015-03-21 23:07:37 +0000

    Add test to ensure that compressed file contents do not depend on input paths.

--- RXPatchLibTest/XdeltaPatchBuilderTest.cs
+++ RXPatchLibTest/XdeltaPatchBuilderTest.cs
@@ -23,5 +23,45 @@ namespace RXPatchLibTest
                 Assert.AreNotEqual(0, new FileInfo(patchFile.Path).Length);
             }
         }
+
+        [TestMethod]
+        public async Task PatchContentsDoNotDependOnInputPath()
+        {
+            var patchBuilder = new XdeltaPatchBuilder(XdeltaPatchSystemFactory.Preferred);
+
+            using (var oldFile1 = new TemporaryFile())
+            using (var newFile1 = new TemporaryFile())
+            using (var oldFile2 = new TemporaryFile())
+            using (var newFile2 = new TemporaryFile())
+            using (var patchFile1 = new TemporaryFile())
+            using (var patchFile2 = new TemporaryFile())
+            {
+                File.WriteAllText(oldFile1.Path, "a");
+                File.WriteAllText(oldFile2.Path, "a");
+                File.WriteAllText(newFile1.Path, "b");
+                File.WriteAllText(newFile2.Path, "b");
+                await patchBuilder.CreatePatchAsync(oldFile1.Path, newFile1.Path, patchFile1.Path);
+                await patchBuilder.CreatePatchAsync(oldFile2.Path, newFile2.Path, patchFile2.Path);
+                CollectionAssert.AreEquivalent(File.ReadAllBytes(patchFile1.Path), File.ReadAllBytes(patchFile2.Path));
+            }
+        }
+
+        [TestMethod]
+        public async Task CompressedContentsDoNotDependOnInputPath()
+        {
+            var patchBuilder = new XdeltaPatchBuilder(XdeltaPatchSystemFactory.Preferred);
+
+            using (var newFile1 = new TemporaryFile())
+            using (var newFile2 = new TemporaryFile())
+            using (var patchFile1 = new TemporaryFile())
+            using (var patchFile2 = new TemporaryFile())
+            {
+                File.WriteAllText(newFile1.Path, "b");
+                File.WriteAllText(newFile2.Path, "b");
+                await patchBuilder.CompressAsync(newFile1.Path, patchFile1.Path);
+                await patchBuilder.CompressAsync(newFile2.Path, patchFile2.Path);
+                CollectionAssert.AreEquivalent(File.ReadAllBytes(patchFile1.Path), File.ReadAllBytes(patchFile2.Path));
+            }
+        }
     }
 }
