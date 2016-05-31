5f61c429939bca0d23e6f2ddead907fee244d628
Jessica James <JAJames>
2016-05-31 19:03:49 +0000

    Removed some dead code

--- Renegade X Launcher/RenXWebLinks.cs
+++ Renegade X Launcher/RenXWebLinks.cs
@@ -7,13 +7,8 @@ using System.Threading.Tasks;
 
     public static class RenXWebLinks
     {
-        public static readonly string[] RENX_SERVER_INFO_BREAK_SYMBOL = new string[1] {"<@>"};
-        public static readonly char RENX_SERVER_INFO_SPACER_SYMBOL = '~';
         public static readonly char RENX_SERVER_SETTING_SPACE_SYMBOL = ';';
-        public const string RENX_SERVER_PAGES_URL = "http://renegadexgs.appspot.com/GetServerPages.jsp";
-        public const string RENX_ACTIVE_SERVERS_LIST_URL = "http://renegadexgs.appspot.com/browser_3.jsp?view=false";
-        public const string RENX_ACTIVE_SERVER_COUNT_URL = "http://renegadexgs.appspot.com/browser_3.jsp?view=true";
-        public const string RENX_ACTIVE_SERVER_JSON_URL = "http://renegadexgs.appspot.com/servers.jsp";
+        public const string RENX_ACTIVE_SERVER_JSON_URL = "http://serverlist.renegade-x.com/servers.jsp";
         public const string RENX_BANNERS_JSON_URL = "http://renegade-x.com/launcher_data/banners";
     }
 
