0aadac036f44c34fd33e8a27650f809306056947
Rob Smit <Schmitzenbergh>
2016-07-19 16:17:57 +0000

    -Fixed a big bug in the seeker that when 'cancel' was pressed, it would continue downloading anyway. Now it stops downloading after the current download is done. (Current means one map) -Started with a UE3Redist download window (Making a download window for general use)

---
+++ Renegade X Launcher/Views/GeneralDownloadWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LauncherTwo.Views
+{
+
+    public partial class GeneralDownloadWindow : RXWindow
+    {
+        
+        private CancellationTokenSource token;
+        private long sizeOfFile;
+
+        internal string Status
+        {
+            get { return StatusLabelContent.Content.ToString(); }
+            set { Dispatcher.Invoke(new Action(() => { StatusLabelContent.Content = value; })); }
+        }
+
+        
+        public GeneralDownloadWindow(CancellationTokenSource token, String windowTitle, long sizeOfFile)
+        {
+            InitializeComponent();
+            this.token = token;
+            this.Title = windowTitle;
+        }
+
+        public void setStatusLabel(String status)
+        {
+            this.StatusLabelContent.Content = status;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.token.Cancel();
+            this.Close();
+        }
+
+        public void ToggleProgressBar()
+        {
+            switch (ProgressBar.Visibility)
+            {
+                case Visibility.Collapsed:
+                    Dispatcher.Invoke(new Action(() => { ProgressBar.Visibility = Visibility.Visible; }));
+                    break;
+
+                case Visibility.Visible:
+                    Dispatcher.Invoke(new Action(() => { ProgressBar.Visibility = Visibility.Collapsed; }));
+                    break;
+            }
+           
+        }
+
+        public void ToggleContinueButton()
+        {
+            switch (ContinueBtn.Visibility)
+            {
+                case Visibility.Collapsed:
+                    Dispatcher.Invoke(new Action(() => { ContinueBtn.Visibility = Visibility.Visible; }));
+                    break;
+
+                case Visibility.Visible:
+                    Dispatcher.Invoke(new Action(() => { ContinueBtn.Visibility = Visibility.Collapsed; }));
+                    break;
+            }
+        }
+
+        public void initProgressBar(long sizeOfFile)
+        {
+            this.sizeOfFile = sizeOfFile;
+            Dispatcher.Invoke(new Action(() => {
+                this.ProgressPercentage.Content = "0%";
+                this.ProgressBar.Maximum = sizeOfFile;
+            }));
+        }
+
+        public void updateProgressBar(long currentAmount)
+        {
+            Dispatcher.Invoke(new Action(() => {
+                this.ProgressBar.Value = currentAmount; 
+                if (this.sizeOfFile != 0)
+                {
+                    this.ProgressPercentage.Content = (int)currentAmount / (this.sizeOfFile / 100) + "%";
+                }
+                else
+                {
+                    this.ProgressPercentage.Content = "0%";
+                }
+            }));
+        }
+
+        
+    }
+}
