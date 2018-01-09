2b35212e8b35f4ecd21e40cf095e80395c408469
Jessica James <jessica.aj@outlook.com>
2018-01-08 23:06:35 -0600

    Updated package.ps1 and .gitignore

--- package.ps1
+++ package.ps1
@@ -1,23 +1,23 @@
-# Copy launcher binaries to /Renegade X Launcher/bin
+# Copy launcher binaries to /bin
 Remove-Item "bin" -Recurse -ErrorAction Ignore
 ROBOCOPY "Renegade X Launcher/bin/Release" "bin" *.dll *.exe *.config /S /XF *.vshost*
 
-# Copy patcher binaries to /RXPatch
-ROBOCOPY "RXPatch/bin/Release" "../RXPatch" *.dll *.exe *.config /S /XF *.vshost*
+# Copy patcher binaries to /RXPatch-bin
+ROBOCOPY "RXPatch/bin/Release" "RXPatch-bin" *.dll *.exe *.config /S /XF *.vshost*
 
 # Get the current SVN revision number
-$revision = ([xml](svn info --xml)).info.entry.revision
+$revision = (git rev-parse --short HEAD)
 
-# Zip "/Renegade X Launcher/bin" to "/Renegade X Launcher/launcher-revision.zip"
+# Zip "/bin" to "/launcher-revision.zip"
 Remove-Item "launcher-*.zip" -ErrorAction Ignore
 $zipTarget = "launcher-$($revision).zip"
 Remove-Item $zipTarget -ErrorAction Ignore
 Add-Type -Assembly System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::CreateFromDirectory( ( join-path (Get-Location) bin ), ( join-path (Get-Location) $zipTarget ), [System.IO.Compression.CompressionLevel]::Optimal, $false)
 
-# Calculate SHA-256 hash of "/Renegade X Launcher/launcher-revision.zip"
+# Calculate SHA-256 hash of "/launcher-revision.zip"
 $hash = ( Get-FileHash -Algorithm SHA256 ./$zipTarget ).Hash
 echo "SHA256 Hash: $hash"
 
-# Write hash to /Renegade X Launcher/bin/launcher-hash.txt
+# Write hash to /bin/launcher-hash.txt
 $hash | Set-Content 'launcher-hash.txt'
