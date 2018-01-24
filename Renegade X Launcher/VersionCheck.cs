5d1121d097a9c60703369c0a46534298f81f2bf9
AlienXAXS <mark.phillips@axsemail.co.uk>
2018-01-24 14:37:48 +0000

    friendly_name -> name

--- Renegade X Launcher/VersionCheck.cs
+++ Renegade X Launcher/VersionCheck.cs
@@ -149,7 +149,7 @@ namespace LauncherTwo
 
                 // Server URL's list & Friendly Names
                 foreach (var x in versionData["game"]["server_urls"].ToObject<dynamic>())
-                    RXPatcher.Instance.AddNewUpdateServer(x["url"].ToString(), x["friendly_name"].ToString());
+                    RXPatcher.Instance.AddNewUpdateServer(x["url"].ToString(), x["name"].ToString());
             }
             catch(Exception ex)
             {
