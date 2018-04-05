41cc1f5a5f3532f4dba4b1570effc19db2f303f5
Jessica James <jessica.aj@outlook.com>
2018-04-04 23:35:15 -0500

    refactored publish.sh to use s3cmd for publishing launcher/installer files

--- packaging/publish.sh
+++ packaging/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# To use this script, define $destination and $path below.
+# To use this script, define $launcher_data_destination and $version_data_destination below.
 # You must also be setup for non-interactive authentication with the user/host
 #
 
@@ -14,10 +14,9 @@ bin=bin/
 launcher=${bin}launcher-$revision.zip
 installer=${bin}Renegade_X_Installer-$revision.msi
 json=${bin}version/*.json
-args='-av --update'
 
 # Connect and transfer over the launcher and installer
-rsync $args $launcher $installer $launcher_data_destination
+s3cmd put $launcher $installer $launcher_data_destination
 
 # Connect and transfer over the version files
-rsync $args $json $version_data_destination
+rsync -av --update $json $version_data_destination
